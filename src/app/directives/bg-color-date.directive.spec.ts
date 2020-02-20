0bd289e2a64ab0154ccf4608e015e6ce8f949189
maxrusmr2011@gmail.com <maxrusmr2011@gmail.com>
2020-02-20 02:02:37 +0200

    feat: add directive

---
+++ src/app/directives/bg-color-date.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BgColorDateDirective } from './bg-color-date.directive';
+
+describe('BgColorDateDirective', () => {
+  it('should create an instance', () => {
+    const directive: BgColorDateDirective = new BgColorDateDirective();
+    expect(directive).toBeTruthy();
+  });
+});
