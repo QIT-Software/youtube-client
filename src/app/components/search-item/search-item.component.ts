596c4ae42a4194c3e6613211ea9ac37ca8600f85
maxrusmr2011@gmail.com <maxrusmr2011@gmail.com>
2020-02-20 02:06:47 +0200

    feat: search-item component

--- src/app/components/search-item/search-item.component.ts
+++ src/app/components/search-item/search-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { IResponseItem } from '../../models/response-item.model';
+import { Component, OnInit, Input } from '@angular/core';
+import { IResponseItem, IThumbnailList, IStatistics } from '../../models/response-item.model';
 
 @Component({
   selector: 'app-search-item',
@@ -7,10 +7,20 @@ import { IResponseItem } from '../../models/response-item.model';
   styleUrls: ['./search-item.component.scss']
 })
 export class SearchItemComponent implements OnInit {
-  public responseItem: IResponseItem;
+  @Input() public responseItem: IResponseItem;
+  public imagesList: IThumbnailList;
+  public stat: IStatistics;
+  public image: string;
+  public title: string;
+  public date: Date;
+  public color: string;
   constructor() { }
 
   public ngOnInit(): void {
+    this.imagesList = this.responseItem.snippet.thumbnails;
+    this.image = this.imagesList.medium.url;
+    this.stat = this.responseItem.statistics;
+    this.title = this.responseItem.snippet.title;
+    this.date = new Date(this.responseItem.snippet.publishedAt);
   }
-
 }
