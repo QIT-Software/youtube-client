8800d1758cedf8a7fe720e1efd2a9217f7c42c2f
maxrusmr2011@gmail.com <maxrusmr2011@gmail.com>
2020-03-05 19:12:41 +0200

    feat: change header component

--- src/app/core/components/header/header.component.spec.ts
+++ src/app/core/components/header/header.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import { SearchDataService, ShowSettingService, RequestHttpService } from '@coreService/index';
+import { CoreModule } from '../../core.module';
+import { Router } from '@angular/router';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { MessageService } from '@coreService/message.service';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      providers: [SearchDataService, ShowSettingService, Router,
+        SearchDataService, RequestHttpService, HttpClient, HttpHandler, MessageService],
+
     })
     .compileComponents();
   }));
--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,30 +1,43 @@
-import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter, AfterViewInit } from '@angular/core';
 import { ShowSettingService } from '@coreService/show-setting.service';
 import { SearchDataService } from '@coreService/search-data.service';
+import { fromEvent } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, AfterViewInit {
   public settingBlock: boolean;
-  public searchWord: string;
+  // public searchWord: string;
   @ViewChild('inputElement') public inputElement: ElementRef;
 
   constructor(private searchData: SearchDataService, private showBlockSet: ShowSettingService) {
-    this.settingBlock = this.showBlockSet.get();
+    this.settingBlock = false;
+    this.showBlockSet.hide();
   }
   public ngOnInit(): void {
+    this.showBlockSet.changeShow.subscribe((val) => {
+      this.settingBlock = val ? this.settingBlock : false;
+    });
   }
+
+  public ngAfterViewInit(): void {
+    fromEvent(this.inputElement.nativeElement, 'keyup')
+      .pipe(debounceTime(1000), map(() => this.inputElement.nativeElement.value))
+      .subscribe((val) => {
+        if (!val) {
+          this.searchData.clear();
+        } else if (val.length > 2) {
+          this.searchData.Http(val);
+        }
+      });
+    this.inputElement.nativeElement.focus();
+  }
+
   public showSetting(): void {
-    this.settingBlock = this.searchData.getResponse() ? this.showBlockSet.toggle() : false;
-  }
-  public search(): void {
-    this.searchWord = this.inputElement.nativeElement.value;
-    if (this.searchWord) {
-      this.searchData.mockHttp();
-    } else {
-      this.searchData.clear();
-    }
+    this.settingBlock = this.searchData.getResponseItems().length &&
+      this.showBlockSet.get() ? !this.settingBlock : false;
   }
 }
